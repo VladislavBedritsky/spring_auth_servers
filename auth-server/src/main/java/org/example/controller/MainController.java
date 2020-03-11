02ace1500ff034eb34052f91b6429f6862c099ce
Vladislav Bedritsky <vladquinn2010@gmail.com>
2020-03-11 20:40:06 +0300

    changed oauth2 version

--- auth-server/src/main/java/org/example/controller/MainController.java
+++ auth-server/src/main/java/org/example/controller/MainController.java
@@ -5,9 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import java.security.Principal;
-
 @RestController
 @RequestMapping("/rest")
 public class MainController {
