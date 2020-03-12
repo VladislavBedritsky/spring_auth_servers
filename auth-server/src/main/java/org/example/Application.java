a4be4479c08d26ffae859ff1202f72329a6855ae
Vladislav Bedritsky <vladquinn2010@gmail.com>
2020-03-12 11:22:41 +0300

    finished with auth and client server

--- auth-server/src/main/java/org/example/Application.java
+++ auth-server/src/main/java/org/example/Application.java
@@ -3,9 +3,12 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
-public class Application {
+@EnableResourceServer
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- auth-server/src/main/java/org/example/controller/MainController.java
+++ auth-server/src/main/java/org/example/controller/MainController.java
@@ -1,9 +1,10 @@
 package org.example.controller;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/rest")
@@ -15,8 +16,8 @@ public class MainController {
     }
 
     @GetMapping("/user")
-    public String name(Authentication authentication) {
-        System.out.println(authentication.getName());
-        return authentication.getName();
+    public Principal getUser(Principal principal) {
+        System.out.println(principal);
+        return principal;
     }
 }
